9fb295d443be95ae7ba020326515e8237aad97ce
pmartinchinonso <promisemary10@gmail.com>
2018-05-04 02:04:40 +0100

    another commit when dealing with models and database

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,6 +11,9 @@
         
     </head>
     <body>
-        @yield('content')
+        @include('inc.navbar')
+        <div class="container">
+            @yield('content')
+        </div>
     </body>
 </html>
--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -1,7 +1,9 @@
 @extends('layouts.app')
 @section('content')
-    <h1>{{$title}}</h1>
-    <p>This is the Laravel application that is been worked on from here.</p>
+    <div class="jumbotron text-center">
+        <h1 class="display-3">{{$title}}</h1>
+        <p>This is the Laravel application that is been worked on from here.</p>
+        <p><a class="btn btn-primary btn-lg" href="/login" role="button">Login &raquo;</a> <a class="btn btn-success btn-lg" href="/register" role="button">Register &raquo;</a></p>
+    </div>
 @endsection
-
 
